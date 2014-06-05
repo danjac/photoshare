6d3977a427a6431af0b5856dc6b6fda406018b07
Dan Jacob <danjac354@gmail.com>
2014-06-05 19:49:15 +0300

    Fixing some files

---
+++ api/routes/auth.go
@@ -0,0 +1,68 @@
+package routes
+
+import (
+	"github.com/danjac/photoshare/api/models"
+	"github.com/danjac/photoshare/api/render"
+	"github.com/danjac/photoshare/api/session"
+	"net/http"
+)
+
+func logout(w http.ResponseWriter, r *http.Request) {
+
+	if err := session.Logout(w); err != nil {
+		render.Error(w, err)
+		return
+	}
+
+	render.Status(w, http.StatusOK, "Logged out")
+
+}
+
+// return current logged in user, or 401
+func authenticate(w http.ResponseWriter, r *http.Request) {
+
+	user, err := session.GetCurrentUser(r)
+	if err != nil {
+		render.Error(w, err)
+		return
+	}
+
+	var status int
+
+	if user != nil {
+		status = http.StatusOK
+	} else {
+		status = http.StatusNotFound
+	}
+
+	render.JSON(w, status, user)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	if email == "" || password == "" {
+		render.Status(w, http.StatusBadRequest, "Email or password missing")
+		return
+	}
+
+	user, err := models.Authenticate(email, password)
+	if err != nil {
+		render.Error(w, err)
+		return
+	}
+
+	if user == nil {
+		render.Status(w, http.StatusNotFound, "No user found")
+		return
+	}
+
+	if err := session.Login(w, user); err != nil {
+		render.Error(w, err)
+		return
+	}
+
+	render.JSON(w, http.StatusOK, user)
+}
---
+++ api/routes/photos.go
@@ -0,0 +1,61 @@
+package routes
+
+import (
+	"github.com/danjac/photoshare/api/models"
+	"github.com/danjac/photoshare/api/render"
+	"github.com/danjac/photoshare/api/session"
+	"github.com/danjac/photoshare/api/utils"
+	"net/http"
+)
+
+func upload(w http.ResponseWriter, r *http.Request) {
+
+	user, err := session.GetCurrentUser(r)
+	if err != nil {
+		render.Error(w, err)
+		return
+	}
+	if user == nil {
+		render.Status(w, http.StatusUnauthorized, "Not logged in")
+		return
+	}
+
+	title := r.FormValue("title")
+	src, hdr, err := r.FormFile("photo")
+	if err != nil {
+		render.Error(w, err)
+		return
+	}
+	contentType := hdr.Header["Content-Type"][0]
+	if contentType != "image/png" && contentType != "image/jpeg" {
+		render.Status(w, http.StatusBadRequest, "Not a valid image")
+		return
+	}
+
+	defer src.Close()
+	filename, err := utils.ProcessImage(src, contentType)
+	if err != nil {
+		render.Error(w, err)
+		return
+	}
+
+	photo := &models.Photo{Title: title,
+		OwnerID: user.ID, Photo: filename}
+
+	if err := photo.Save(); err != nil {
+		render.Error(w, err)
+		return
+	}
+
+	render.JSON(w, http.StatusOK, photo)
+}
+
+func getPhotos(w http.ResponseWriter, r *http.Request) {
+
+	photos, err := models.GetPhotos()
+	if err != nil {
+		render.Error(w, err)
+		return
+	}
+	render.JSON(w, http.StatusOK, photos)
+}

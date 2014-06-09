52a3b3ad0b36245a48f512f7db158c1c5685e141
Dan Jacob <danjac354@gmail.com>
2014-06-09 23:06:08 +0300

    Remove unneeded struct

--- api/routes/context.go
+++ api/routes/context.go
@@ -28,10 +28,12 @@ func (c *AppContext) GetCurrentUser() (*models.User, error) {
 }
 
 func (c *AppContext) Login(user *models.User) error {
+    c.User = user
 	return session.Login(c.Response, user)
 }
 
 func (c *AppContext) Logout() error {
+    c.User = nil
 	return session.Logout(c.Response)
 }
 

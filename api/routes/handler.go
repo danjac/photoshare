da865f6dc2cb09ad1416c42212c7c418fb390afc
Dan Jacob <danjac354@gmail.com>
2014-06-21 16:31:26 +0300

    Add allow headers

--- api/routes/handler.go
+++ api/routes/handler.go
@@ -133,7 +133,6 @@ type AppHandlerFunc func(c *Context) *Result
 
 func MakeAppHandler(fn AppHandlerFunc, loginRequired bool) http.HandlerFunc {
 
-
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		var result *Result
@@ -143,6 +142,21 @@ func MakeAppHandler(fn AppHandlerFunc, loginRequired bool) http.HandlerFunc {
 				log.Println(r)
 			}
 		}()
+
+		// basic setup
+
+		r.ParseForm()
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+			w.Header().Set("Access-Control-Max-Age", "1000")
+			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Access-Token, X-Requested-With, Content-Type, Accept")
+			return
+		}
+
+		// new Context
 
 		c := NewContext(w, r)
 

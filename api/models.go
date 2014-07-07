24c7fd6f5df5ad9d1daf54c577284a850684230d
Dan Jacob <danjac354@gmail.com>
2014-07-08 00:37:43 +0300

    Moving some funcs to utils

--- api/models.go
+++ api/models.go
@@ -11,7 +11,6 @@ import (
 	"log"
 	"math"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -588,27 +587,4 @@ func (user *User) GetVotes() []int64 {
 
 func (user *User) SetVotes(votes []int64) {
 	user.Votes = intSliceToPgArr(votes)
-}
-
-// Converts a Pg Array (returned as string) to an int slice
-func pgArrToIntSlice(pgArr string) []int64 {
-	var items []int64
-
-	s := strings.TrimRight(strings.TrimLeft(pgArr, "{"), "}")
-
-	for _, value := range strings.Split(s, ",") {
-		if item, err := strconv.Atoi(value); err == nil {
-			items = append(items, int64(item))
-		}
-	}
-	return items
-}
-
-// Converts an int slice to a Pg Array string
-func intSliceToPgArr(items []int64) string {
-	var s []string
-	for _, value := range items {
-		s = append(s, strconv.FormatInt(value, 10))
-	}
-	return "{" + strings.Join(s, ",") + "}"
 }
--- api/utils.go
+++ api/utils.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"path"
+	"strconv"
+	"strings"
 	"text/template"
 )
 
@@ -25,4 +27,27 @@ func baseURL(r *http.Request) string {
 
 func parseTemplate(name string) *template.Template {
 	return template.Must(template.ParseFiles(path.Join(Config.Dirs.Templates, name)))
+}
+
+// Converts a Pg Array (returned as string) to an int slice
+func pgArrToIntSlice(pgArr string) []int64 {
+	var items []int64
+
+	s := strings.TrimRight(strings.TrimLeft(pgArr, "{"), "}")
+
+	for _, value := range strings.Split(s, ",") {
+		if item, err := strconv.Atoi(value); err == nil {
+			items = append(items, int64(item))
+		}
+	}
+	return items
+}
+
+// Converts an int slice to a Pg Array string
+func intSliceToPgArr(items []int64) string {
+	var s []string
+	for _, value := range items {
+		s = append(s, strconv.FormatInt(value, 10))
+	}
+	return "{" + strings.Join(s, ",") + "}"
 }

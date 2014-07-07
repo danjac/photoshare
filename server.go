9f7fbde7c2a0449fd15acca5f34c012f6353a400
Dan Jacob <danjac354@gmail.com>
2014-07-08 00:23:13 +0300

    Refactoring done

--- server.go
+++ server.go
@@ -4,9 +4,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"github.com/danjac/photoshare/api/config"
-	"github.com/danjac/photoshare/api/models"
-	_ "github.com/danjac/photoshare/api/routes"
+	"github.com/danjac/photoshare/api"
 	"github.com/zenazn/goji"
 	"log"
 	"net/http"
@@ -18,10 +16,10 @@ func main() {
 	runtime.GOMAXPROCS((runtime.NumCPU() * 2) + 1)
 
 	db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s host=%s",
-		config.DB.User,
-		config.DB.Name,
-		config.DB.Password,
-		config.DB.Host,
+		api.Config.DB.User,
+		api.Config.DB.Name,
+		api.Config.DB.Password,
+		api.Config.DB.Host,
 	))
 
 	if err != nil {
@@ -30,14 +28,14 @@ func main() {
 
 	defer db.Close()
 
-	if _, err := models.InitDB(db, config.DB.LogSql); err != nil {
+	if _, err := api.InitDB(db, api.Config.DB.LogSql); err != nil {
 		log.Fatal(err)
 	}
 
-	flag.Set("bind", fmt.Sprintf("localhost:%d", config.Server.Port))
+	flag.Set("bind", fmt.Sprintf("localhost:%d", api.Config.Server.Port))
 
 	// for local development
-	goji.Get("/*", http.FileServer(http.Dir(config.Dirs.Public)))
+	goji.Get("/*", http.FileServer(http.Dir(api.Config.Dirs.Public)))
 	goji.Serve()
 
 }

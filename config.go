f62815f5de6c9bd764f679ec10c6651df6287a64
Dan Jacob <danjac354@gmail.com>
2014-07-30 00:18:43 +0300

    Separate settings file

--- config.go
+++ config.go
@@ -2,52 +2,11 @@ package photoshare
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	"github.com/coopernurse/gorp"
-	"github.com/danryan/env"
 	"github.com/gorilla/mux"
 	"net/http"
-	"os"
-	"path"
 )
-
-type settings struct {
-	DBName     string `env:"key=DB_NAME required=true"`
-	DBUser     string `env:"key=DB_USER required=true"`
-	DBPassword string `env:"key=DB_PASS required=true"`
-	DBHost     string `env:"key=DB_HOST default=localhost"`
-
-	TestDBName     string `env:"key=TEST_DB_NAME"`
-	TestDBUser     string `env:"key=TEST_DB_USER"`
-	TestDBPassword string `env:"key=TEST_DB_PASS"`
-	TestDBHost     string `env:"key=TEST_DB_HOST"`
-
-	LogSql bool `env:"key=LOG_SQL default=false"`
-
-	SmtpName          string `env:"key=SMTP_NAME"`
-	SmtpPassword      string `env:"key=SMTP_PASS"`
-	SmtpUser          string `env:"key=SMTP_USER"`
-	SmtpHost          string `env:"key=SMTP_HOST default=localhost"`
-	SmtpPort          int    `env:"key=SMTP_PORT default=25"`
-	SmtpDefaultSender string `env:"key=DEFAULT_EMAIL_SENDER default=webmaster@localhost"`
-
-	BaseDir       string `env:"key=BASE_DIR"`
-	PublicDir     string `env:"key=PUBLIC_DIR"`
-	UploadsDir    string `env:"key=UPLOADS_DIR"`
-	ThumbnailsDir string `env:"key=THUMBNAILS_DIR"`
-	TemplatesDir  string `env:"key=TEMPLATES_DIR"`
-
-	PrivateKey string `env:"key=PRIVATE_KEY required=true"`
-	PublicKey  string `env:"key=PUBLIC_KEY required=true"`
-
-	MemcacheHost string `env:"key=MEMCACHE_HOST default=0.0.0.0:11211"`
-
-	GoogleClientID string `env:"key=GOOGLE_CLIENT_ID"`
-	GoogleSecret   string `env:"key=GOOGLE_SECRET"`
-
-	ServerPort int `env:"key=PORT default=5000"`
-}
 
 type appConfig struct {
 	*settings
@@ -62,57 +21,11 @@ type appConfig struct {
 
 func newAppConfig() (*appConfig, error) {
 
-	settings := &settings{}
-
-	if err := env.Process(settings); err != nil {
+	settings, err := newSettings()
+	if err != nil {
 		return nil, err
 	}
-
 	config := &appConfig{settings: settings}
-
-	if config.TestDBName == "" {
-		config.TestDBName = config.DBName + "_test"
-	}
-
-	if config.TestDBUser == "" {
-		config.TestDBUser = config.DBUser
-	}
-
-	if config.TestDBPassword == "" {
-		config.TestDBPassword = config.DBPassword
-	}
-
-	if config.TestDBHost == "" {
-		config.TestDBHost = config.DBHost
-	}
-
-	if config.TestDBName == config.DBName {
-		return config, errors.New("test DB name same as DB name")
-	}
-
-	if config.BaseDir == "" {
-		config.BaseDir = getDefaultBaseDir()
-	}
-
-	if config.PublicDir == "" {
-		config.PublicDir = path.Join(config.BaseDir, "public")
-	}
-
-	if config.UploadsDir == "" {
-		config.UploadsDir = path.Join(config.PublicDir, "uploads")
-	}
-
-	if config.ThumbnailsDir == "" {
-		config.ThumbnailsDir = path.Join(config.UploadsDir, "thumbnails")
-	}
-
-	if config.TemplatesDir == "" {
-		config.TemplatesDir = path.Join(config.BaseDir, "templates")
-	}
-
-	if err := config.initDB(); err != nil {
-		return config, err
-	}
 
 	if err := config.initDB(); err != nil {
 		return config, err
@@ -219,12 +132,4 @@ func (config *appConfig) getRouter() http.Handler {
 
 	return r
 
-}
-
-func getDefaultBaseDir() string {
-	defaultBaseDir, err := os.Getwd()
-	if err != nil {
-		defaultBaseDir = "."
-	}
-	return defaultBaseDir
 }

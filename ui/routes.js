c4134dc9096d4291a95216b8e1b41719e98b3bd4
Dan Jacob <danjac354@gmail.com>
2015-08-29 01:55:55 +0300

    Navbar

--- ui/routes.js
+++ ui/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { DefaultRoute, Route } from 'react-router';
 
-import { App, Popular } from './handlers';
+import { App, Popular, Latest } from './handlers';
 
 export default (
   <Route name="app" path="/" handler={App}>
     <DefaultRoute handler={Popular} />
+    <Route name="latest" handler={Latest} />
   </Route>
 );
